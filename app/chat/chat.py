65d96334808f614fd4e2689e70c86e0ff05b628a
mostalt <mostalt.mail@gmail.com>
2023-12-30 14:15:28 +0700

    streamable chain

--- app/chat/chat.py
+++ app/chat/chat.py
@@ -1,5 +1,6 @@
-from  langchain.chains import ConversationalRetrievalChain
+from langchain.chat_models import ChatOpenAI
 
+from app.chat.chains.retrieval import StreamingConversationalRetrievalChain
 from app.chat.models import ChatArgs
 from app.chat.vector_stores.pinecone import build_retriever
 from app.chat.llms.chat_openai import build_llm
@@ -9,10 +10,12 @@ from app.chat.memories.sql_memory import build_memory
 def build_chat(chat_args: ChatArgs):
    retriever = build_retriever(chat_args)
    llm = build_llm(chat_args)
+   condence_question_llm = ChatOpenAI(streaming=False)
    memory=build_memory(chat_args)
 
-   return ConversationalRetrievalChain.from_llm(
+   return StreamingConversationalRetrievalChain.from_llm(
        llm=llm,
+       condense_question_llm=condence_question_llm,
        memory=memory,
        retriever=retriever
    )
