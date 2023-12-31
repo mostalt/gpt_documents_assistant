969244b1763fd15ae4f2ffdfd64387ab96c773e9
mostalt <mostalt.mail@gmail.com>
2023-12-31 11:41:38 +0700

    window buffer memory component

--- app/chat/memories/sql_memory.py
+++ app/chat/memories/sql_memory.py
@@ -6,27 +6,12 @@ from app.web.api import (
   get_messages_by_conversation_id,
   add_message_to_conversation
 )
+from app.chat.memories.histories.sql_history import SqlMessageHistory
 
-class SqlMessageHistory(BaseChatMessageHistory, BaseModel):
-  conversation_id: str
-  
-  @property
-  def messages(self):
-    return get_messages_by_conversation_id(self.conversation_id)
 
-  def add_message(self, message):
-    return add_message_to_conversation(
-      conversation_id=self.conversation_id,
-      role=message.type,
-      content=message.content
-    )
-
-  def clear():
-    pass
-
-def build_memory(char_args):
+def build_memory(chat_args):
   return ConversationBufferMemory(
-    chat_memory=SqlMessageHistory(conversation_id=char_args.conversation_id),
+    chat_memory=SqlMessageHistory(conversation_id=chat_args.conversation_id),
     return_messages=True,
     memory_key="chat_history",
     output_key="answer"
